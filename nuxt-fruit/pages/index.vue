1e503a03fdf75089b662592c3b1ea469c89d33d0
Prashannjeet Paul <prashannjeet.paul@olacabs.com>
2022-08-01 17:31:31 +0530

    signUp axios

--- nuxt-fruit/pages/index.vue
+++ nuxt-fruit/pages/index.vue
@@ -2,18 +2,44 @@
 
 <template>
 <fieldset>
-    <legend><h2>login</h2></legend>
-    <form id="login">
-     <input type="text" id="userName" name="name" placeholder="User Name"><br><br>
-    <input type="password" id="password" name="name" placeholder="password"><br><br>
-    <button type="Submit">Submit</button>
+    <legend><h2>SignUp</h2></legend>
+    <form id="signUp">
+     <input type="text" v-model='name' id="userName" name="name" placeholder="User Name"><br><br>
+    <input type="email" v-model="email" id="email" name="name" placeholder="Email"><br><br>
+    <input type="password" v-model="password" id="password" name="name" placeholder="password"><br><br>
+    <button @click="signUp">signup</button>
     </form>
 </fieldset>
 </template>
 <script>
+import axios from 'axios'
+export default {
+    name:'signUp',
+    data(){
+        return {
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        async signUp(){
+            console.log(this.name,this.email,this.password)
+            let result=await axios.post('http://localhost:3001/',{
+                name:this.name,
+                email:this.email,
+                password:this.password
+            });
+            console.log(result)
+            if(result.status==201){
+                console.log('signup done')
+            }
+        }
+    }
+}
 </script>
 <style>
-#login input{
+#signUp input{
     width: 300px;
     height:40px;
     padding-left: 20px;
@@ -22,5 +48,13 @@
     margin-right: auto;
     border:1px solid skyblue
 
+}
+#signUp button{
+    width: 320px;
+    height:40px;
+    border: 1px solid skyblue;
+    color:#fff;
+    background-color: skyblue;
+    cursor: pointer;
 }
 </style>
