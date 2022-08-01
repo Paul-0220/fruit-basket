d5a8a71e7634148877702f22c7c6c13b02492af5
Prashannjeet Paul <prashannjeet.paul@olacabs.com>
2022-08-01 14:51:43 +0530

    login Page

---
+++ nuxt-fruit/pages/index.vue
@@ -0,0 +1,16 @@
+<!-- SignUp page -->
+
+<template>
+<fieldset>
+    <legend><h2>login</h2></legend>
+    <form id="login">
+     <input type="text" id="userName" name="name" placeholder="User Name"><br><br>
+    <input type="password" id="password" name="name" placeholder="password"><br><br>
+    <button type="Submit">Submit</button>
+    </form>
+</fieldset>
+</template>
+<script>
+</script>
+<style>
+</style>
